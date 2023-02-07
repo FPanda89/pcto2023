5a4358d6dafaabb86c934efd2766faa1f35fa2f0
FPanda89 <124669001+FPanda89@users.noreply.github.com>
2023-02-07 14:51:41 +0100

    Add files via upload

---
+++ Fede-3.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+
+
+printf(" inserire l'eta dell'utente\n");
+scanf("%d", &a );
+
+if (a >= 18)
+{
+    printf("l'utente é maggiorenne \n");
+}
+else 
+{
+    printf("l'utente é minorenne\n");
+
+}
+}
+
+
+
