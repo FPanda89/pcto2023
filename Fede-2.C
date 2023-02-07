d72c38b53f64e92349e1866c5a6f397f55d885d2
FPanda89 <124669001+FPanda89@users.noreply.github.com>
2023-02-07 12:38:09 +0100

    Add files via upload

---
+++ Fede-2.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+ int a;
+ int b;
+
+ printf("inserisci due interi: %d %d\n", a , b);
+ scanf("%d %d", &a , &b);
+
+ if (a > b)
+ {
+    printf("a é maggiore di b\n");
+   
+ }
+ else if (a == b)
+ {
+    printf("a é uguale a b\n");
+   
+ }
+ else if (a < b)
+ {
+    printf("a é minore di b\n");
+   
+ }
+ else
+}
+
