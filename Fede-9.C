a7d10c621283e90f179177f97ffb7a5a86c35608
FPanda89 <124669001+FPanda89@users.noreply.github.com>
2023-02-08 10:26:17 +0100

    Add files via upload

--- Fede-9.C
+++ Fede-9.C
@@ -9,18 +9,27 @@ int c;
 printf("inserisci tre interi\n");
 scanf("%d %d %d", &a , &b , &c);
 
-
-if (a == b &&  a == c && b == c)
+if (a + b > c && b + c > a && a + c > b )
 {
+    printf("é un triangolo\n");
+    
+     if (a == b && a == c)
+    {
     printf("é un triangolo equilatero\n");
-}
-else if ((a == b && a != c) || (b == c && a !=c) || ( c == a && a !=b))
-{
-    printf("é un triangolo isoscele\n");
-}
-else if ( a != b && a != c && b != c)
-{
+    }
+
+    else if ((a == b && a != c) || (b == c && a !=c) || ( c == a && a !=b))
+    {
+        printf("é un triangolo isoscele\n");
+    }
+    else if ( a != b && a != c && b != c)
+    {
     printf("é un triangolo scaleno\n");
+    }
+}
+else 
+{
+    printf("non é un triangolo\n");
 }
 
 }
