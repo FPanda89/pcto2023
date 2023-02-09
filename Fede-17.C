47d654240bb7b1ad74d0fe579b64c823ddbef8b1
FPanda89 <124669001+FPanda89@users.noreply.github.com>
2023-02-09 11:05:02 +0100

    Add files via upload

---
+++ Fede-17.C
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+ float x;
+ float y;
+ float p = 1;
+ printf("inserisci due numeri\n");
+ scanf("%f %f", &x , &y);
+ 
+ if (y > 0)
+ {
+    while (y != 0)
+   {
+      p = p * x;
+      y =  y - 1;
+   }
+   printf("potenza %f\n", p);
+   }
+   else if (y < 0)
+   {
+      x = 1 / x;
+      while (y != 0)
+      {
+         p = p * x;
+         y =  y + 1;
+      }
+      printf("potenza %f\n", p);
+   }
+ 
+ 
+ }
+
+
+   
+
+
+
+
+
+
+
+
+
+
